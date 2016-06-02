d9fc9ab0e9ae9b77bdf9a79e96cdefadb98661d3
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2016-06-02 12:28:44 -0400

    Install browsersync globally

--- install.sh
+++ install.sh
@@ -26,6 +26,9 @@ mv composer.phar /usr/local/bin/composer
 /usr/local/bin/composer global require hirak/prestissimo
 /usr/local/bin/composer global require laravel/installer laravel/lumen-installer squizlabs/php_codesniffer phpmd/phpmd laravel/valet
 
+# Install global Node packages
+npm install -g browser-sync
+
 # Install Laravel Valet
 valet install
 
