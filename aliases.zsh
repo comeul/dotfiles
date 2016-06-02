a36110f6bb282456d9d784a8c363a9058ef3bec5
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2016-06-02 12:27:23 -0400

    Removed homestead aliases since I use Valet + correct path to project

--- aliases.zsh
+++ aliases.zsh
@@ -2,16 +2,9 @@ alias slt='open -a "Sublime Text"'
 alias zshconfig="slt ~/.zshrc"
 alias envconfig="slt ~/.env.sh"
 
-#Virtual machines
-alias homestead='function __homestead() { (cd ~/homestead && vagrant $*); unset -f __homestead; }; __homestead'
-alias homestead56='function __homestead56() { (cd ~/Projets/homestead56 && vagrant $*); unset -f __homestead56; }; __homestead56'
-alias elastic='function __elastic() { (cd ~/elasticsearch && vagrant $*); unset -f __elastic; }; __elastic'
-alias vmup='homestead up && homestead56 up && elastic up'
-alias vmdown='homestead halt && homestead56 halt && elastic halt'
-
 #Laravel
 alias artisan="php artisan"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
-alias projets="cd $HOME/Projets"
+alias sites="cd $HOME/Sites"
